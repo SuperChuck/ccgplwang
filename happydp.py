e7ff72ab72f342f29c1f52e1e024e30c0f34ea66
Chuck <chen.changge@163.com>
2015-03-18 15:42:50 +0800

    the origin test.py and the pipeline in happydp.py

---
+++ happydp.py
@@ -0,0 +1,9 @@
+#happy discourse parsing
+
+#explict connective(45.25%)
+#### connective classifier
+#### top level sense classifier
+#### Argument Labeler(CRF)
+
+#implict connective(40.44/73.86)&EntRel(12.7/23.20)&AltLex(1.61/2.94)
+####
--- test.py
+++ test.py
@@ -1,3 +1,20 @@
 import os,sys
+import json
 
-print('Try Git on this one...')
+pdtb_file = open('data/trial/pdtb_trial_data.json')
+
+relations = [ json.loads(x) for x in pdtb_file ]
+print len(relations)
+example_relation = relations[10]
+
+parse_file = open('data/trial/pdtb_trial_parses.json')
+parse_dict = json.loads(parse_file.read())
+doc_id = example_relation['DocID']
+#the constituent parse
+print parse_dict[doc_id]['sentences'][15]['parsetree']
+#get the dependency parse
+#(dependency type, HEAD token-token position, DEPENDENT token-token position)
+print parse_dict[doc_id]['sentences'][15]['dependencies']
+#get part of speech tags
+print parse_dict[doc_id]['sentences'][15]['words'][0][1]['PartOfSpeech']
+print parse_dict[doc_id]['sentences'][15]['words'][1]
